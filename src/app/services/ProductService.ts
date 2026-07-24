46773c194e84c786a799961565e6c863b20ffd0a
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-07-25 00:03:01 +0900

    fix: modify ChosenProduct component logic

--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -17,7 +17,19 @@ class ProductService {
       if (input.search) url += `&search=${input.search}`;
 
       const result = await axios.get(url);
+      console.log("getProducts:", result);
+      return result.data;
+    } catch (err) {
+      console.log("Error, getProducts:", err);
+      throw err;
+    }
+  }
+  public async getProduct(productId: string): Promise<Product> {
+    try {
+      const url = `${this.path}/product/${productId}`;
+      const result = await axios.get(url, { withCredentials: true });
       console.log("getProduct:", result);
+
       return result.data;
     } catch (err) {
       console.log("Error, getProduct:", err);
