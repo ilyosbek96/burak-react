cfc49a87788f5c6e54cbc68da5f7742d73e4e79c
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-07-27 19:36:07 +0900

    feat: publish authentication model component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,17 +8,24 @@ import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer";
 import HelpPage from "./screens/helpPage";
+import Test from "./screens/Test";
+import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
-import Test from "./screens/Test";
-import useBasket from "./hooks/useBasket";
 
 function App() {
   const location = useLocation();
 
   /** =============  HOOKS ========== **/
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+  /** <==================> HANDLERS <==================>**/
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
 
   return (
     <>
@@ -57,6 +64,13 @@ function App() {
         </Route>
       </Switch>
       <Footer />
+
+      <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleSignupClose={handleSignupClose}
+        handleLoginClose={handleLoginClose}
+      />
     </>
   );
 }
