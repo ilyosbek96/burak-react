64a1d29f89297b39f4c89c25bc7468d7fc3a260b
ilyosbek96 <ilyosbekk1996@gmail.com>
2026-07-31 23:49:54 +0900

    fix: modify ProcessOrder and FinishOrders compoinents

--- src/app/screens/ordersPage/PausedOrders.tsx
+++ src/app/screens/ordersPage/PausedOrders.tsx
@@ -18,6 +18,8 @@ const pausedOrdersRetriever = createSelector(
 
 export default function PausedOrders() {
   const { pausedOrders } = useSelector(pausedOrdersRetriever);
+
+  /** <========== HANDLERs ==========> */
   return (
     <TabPanel value={"1"}>
       <Stack>
